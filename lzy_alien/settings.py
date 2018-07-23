9585cfe5ed020dc6e509a0744cca2370a3ebf2f1
ClodWinter <42426324@qq.com>
2018-07-23 21:32:18 +0800

    合并
    合并

--- lzy_alien/settings.py
+++ lzy_alien/settings.py
@@ -2,8 +2,8 @@ class Settings():
 	"""docstring for ClassName"""
 	def __init__(self):
 		super(Settings, self).__init__()
-		self.screen_width = 1200
-		self.screen_height = 800
+		self.screen_width = 800
+		self.screen_height = 600
 		self.bg_color = (230,230,230)
 		self.ship_speed = 5
 
