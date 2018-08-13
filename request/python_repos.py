57f6b9daec18d810411c22239b15958fce674d1b
李志愉 <lizhiyu@lizhiyudeiMac.local>
2018-08-13 17:23:06 +0800

    更改图表样式
    更改图表样式

--- request/python_repos.py
+++ request/python_repos.py
@@ -1,16 +1,35 @@
 import requests
-
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(url)
 dictResponse = r.json()
 
 if r.status_code == 200:
-	print(dictResponse["total_count"])
+	names = []
+	stars = []
 	items = dictResponse["items"]
 	for item in items:
-		print('\nName:', item['name']) 
-		print('Owner:', item['owner']['login']) 
-		print('Stars:', item['stargazers_count']) 
-		print('Repository:', item['html_url']) 
-		print('Description:', item['description'])
+		names.append(item['name'])
+		stars.append(item['stargazers_count'])
+
+
+
+	my_config = pygal.Config()
+	my_config.x_label_rotation = 45
+	my_config.show_legend = False
+	my_config.title_font_size = 24
+	my_config.label_font_size = 14
+	my_config.major_label_font_size = 18
+	my_config.truncate_label = 15
+	my_config.show_y_guides = False
+	my_config.width = 1000
+
+	my_style = LS("#333366",base_style=LCS)
+	chart= pygal.Bar(my_config,style = my_style)
+	chart.title = 'GitHub'
+	chart.x_labels = names
+
+	chart.add('',stars)
+	chart.render_to_file('python_repos.svg')
