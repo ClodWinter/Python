9ba6447dff36d8f4adb5cd42994b5973e283f6e0
李志愉 <lizhiyu@lizhiyudeiMac.local>
2018-08-16 17:25:51 +0800

    创建网页
    创建网页

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -6,20 +6,24 @@ class Topic(models.Model):
 	text = models.CharField(max_length=200)
 	date_added = models.DateTimeField(auto_now_add=True)
 
-	def _str_(self):
+	def __str__(self):
 		return self.text
+
 
 
 class Entry(models.Model):
 	"""docstring for Entry"""
-	topic = models.ForeignKey(Topic)
+	topic = models.ForeignKey(Topic,on_delete=models.CASCADE)#django 2.1后需要传递on_delete
 	text = models.TextField()#没有字数限制的字符串
 	date_added = models.DateTimeField(auto_now_add=True)
 
 	class Meta:
 		verbose_name_plural = 'entries'
 
-def _str_(self):
-	return self.text
+	def __str__(self):
+		if len(self.text)>50:
+			return self.text[:50] + "..."
+		else:
+			return self.text
 		
 		
---
+++ learning_log/learning_logs/urls.py
@@ -0,0 +1,8 @@
+#定义learning_logs的Url模式
+
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+	url(r'^$',views.index,name='index')
+]
